7a578585fac81bca0aa44fd797a3e34cdb52e22b
BuildTools <unconfigured@null.spigotmc.org>
2019-12-28 21:07:55 +0100

    Add navbar logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
-                    {{ config('app.name') }}
+                    <img src="{{ asset('logo/apple-touch-icon.png') }}" alt="{{ config('app.name') }}" width="30" height="30" class="d-inline-block align-top"> {{ config('app.name') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
