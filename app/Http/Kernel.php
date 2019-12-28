0c7b10fc34b791021c7bde5a06c0c13b7ee7b5f5
BuildTools <unconfigured@null.spigotmc.org>
2019-12-28 20:43:02 +0100

    Redirect to HTTPS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 
-        MyApp\Http\Middleware\HttpsProtocol::class,
+        \App\Http\Middleware\HttpsProtocol::class,
     ];
 
     /**
