aa4eb2e0e8c1c12102b9e98e57284f7e5efa6fea
Baranyai Máté <mateb06@gmail.com>
2020-04-08 16:40:27 +0200

    Update to Laravel 7

--- config/app.php
+++ config/app.php
@@ -228,7 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Cloudder' => JD\Cloudder\Facades\Cloudder::class,
-        
+
     ],
 
 ];
