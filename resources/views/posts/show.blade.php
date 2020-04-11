bd7278b03f80428b2d9e400f542b77516be50446
Baranyai Máté <mateb06@gmail.com>
2020-04-11 14:08:01 +0200

    Update post page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,40 +1,31 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-
-    <div class="row">
-        <div class="col-8">
-            <img src="{{ $post->image }}" alt="{{ $post->caption }}" class="w-100">
-        </div>
-        <div class="col-4">
-            <div>
-                <div class="d-flex align-items-center">
-                    <div class="pr-3">
-                        <img src="{{ $post->user->profile->profileImage() }}" alt="" class="w-100 rounded-circle" style="max-width: 40px">
-                    </div>
-                    <div>
-                        <div class="font-weight-bold">
-                            <a class="text-dark" href="/profile/{{ $post->user->id }}">{{ $post->user->username }}</a>
-                        </div>
+@extends('layouts.app') @section('content')
+<div class="container" style="max-width: 900px;">
+    <div class="bg-white rounded shadow">
+        <div class="row m-0">
+            <div class="d-flex align-items-center m-3">
+                <div class="pr-3">
+                    <img src="{{ $post->user->profile->profileImage() }}" alt="" class="w-100 rounded-circle"
+                        style="max-width: 40px">
+                </div>
+                <div>
+                    <div class="font-weight-bold">
+                        <a class="text-dark" href="/profile/{{ $post->user->id }}">{{ $post->user->username }}</a>
                     </div>
                 </div>
+            </div>
+        </div>
 
-                <hr>
+        <div class="row m-0">
+            <div class="col-md-8 p-0 px-3 px-md-0">
+                <img src="{{ $post->image }}" alt="{{ $post->caption }}" class="w-100 rounded">
+            </div>
 
-                <p>
-                    <span class="font-weight-bold">
-                        <a class="text-dark" href="/profile/{{ $post->user->id }}">{{ $post->user->username }}</a>
-                    </span>
+            <div class="col-md-4 p-0">
+                <p style="text-align: justify" class="px-3 py-2 py-md-0">
                     {{ $post->caption }}
                 </p>
-
-                <hr>
-
-                
             </div>
         </div>
     </div>
-
 </div>
 @endsection
