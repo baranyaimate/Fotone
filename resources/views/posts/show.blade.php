b194718428390441030e0d3b14505cd15efdaa42
Baranyai Máté <mateb06@gmail.com>
2020-04-15 22:36:19 +0200

    Update post page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,30 +1,21 @@
 @extends('layouts.app') @section('content')
 <div class="container" style="max-width: 900px;">
-    <div class="bg-white rounded shadow post-card">
-        <div class="row m-0">
-            <div class="d-flex align-items-center m-3">
-                <div class="pr-3">
-                    <img src="{{ $post->user->profile->profileImage() }}" alt="" class="w-100 rounded-circle"
-                        style="max-width: 40px">
-                </div>
-                <div>
-                    <div class="font-weight-bold">
-                        <a class="text-dark" href="/profile/{{ $post->user->id }}">{{ $post->user->username }}</a>
-                    </div>
-                </div>
-            </div>
+    <div class="index-post-card col-md-9 mx-auto mb-4 p-0 border bg-white rounded shadow">
+        <div class="p-3">
+            <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle pr-1"
+                alt="{{ $post->user->username }}" style="max-width: 45px">
+            <span class="font-weight-bold ml-1">
+                <a class="text-dark" href="/profile/{{ $post->user->id }}">{{ $post->user->username }}</a>
+            </span>
+
         </div>
-
-        <div class="row m-0">
-            <div class="col-md-8 p-0 px-3 px-md-0">
-                <img src="{{ $post->image }}" alt="{{ $post->caption }}" class="w-100 rounded">
-            </div>
-
-            <div class="col-md-4 p-0">
-                <p style="text-align: justify" class="px-3 py-2 py-md-0">
-                    {{ $post->caption }}
-                </p>
-            </div>
+        <div>
+            <img src="{{ $post->image }}" alt="{{ $post->caption }}" class="w-100">
+        </div>
+        <div class="p-3">
+            <p class="text-justify m-0">
+                {{ $post->caption }}
+            </p>
         </div>
     </div>
 </div>
