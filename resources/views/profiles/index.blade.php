8cca26622b0a6c1ff04e8d747497710f2e7f9f20
Baranyai Máté <mateb06@gmail.com>
2020-09-28 21:39:10 +0200

    New profile page design

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,17 +9,14 @@
         </div>
     </div>
     <div class="row">
-        <div class="pt-4 w-100">
+        <div class="pt-2 w-100">
             <div class="pb-2">
                 <h3 class="text-center h3">{{ $user->username }}</h3>
-                @cannot('update', $user->profile)
-                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
-                @endcan
             </div>
 
-            <div class="d-flex justify-content-center">
+            <div class="row justify-content-center">
 
-                <div class="mx-2 text-center simple-text"><strong>{{ $postCount }}</strong>
+                <div class="col-lg-2 col-3 text-center card mx-2 py-2"><strong>{{ $postCount }}</strong>
                     @if($postCount <= 1) 
                         post
                     @else
@@ -27,28 +24,35 @@
                     @endif
                 </div>
 
-                <div class="mx-2 text-center">
+                <div class="col-lg-2 col-3 text-center card mx-2 py-2">
                     <a href="{{ route('profile.showFollowers', $user->id) }}" class="no-a-styling">
-                        <strong>{{ $followersCount }}</strong> followers
+                        <strong>{{ $followersCount }}</strong><br>followers
                     </a>
                 </div>
 
-                <div class="mx-2 text-center">
+                <div class="col-lg-2 col-3 text-center card mx-2 py-2">
                     <a href="{{ route('profile.showFollowing', $user->id) }}" class="no-a-styling">
-                        <strong>{{ $followingCount }}</strong> following
+                        <strong>{{ $followingCount }}</strong><br>following
                     </a>
                 </div>
 
             </div>
 
-            <p class="text-center mb-0">{{ $user->profile->description }}</p>
+            <p class="text-center mx-auto my-3 mw-650 ">{{ $user->profile->description }}</p>
             <a class="text-center d-block font-weight-bold" href="{{ $user->profile->url }}">{{ $user->profile->url }}</a>
 
         </div>
 
     </div>
 
+    @cannot('update', $user->profile)
+        <div class="follow-button">
+            <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
+        </div>
+    @endcan
+
     <hr>
+
     @if (!$user->posts->isEmpty())
         @foreach($user->posts->chunk(3) as $chunk)
             <div class="row">
@@ -69,7 +73,7 @@
             </div>
         @endforeach
     @else
-        <h3 class="text-center w-100">No posts yet</h3>
+        <h3 class="text-center w-100 my-5">No posts yet</h3>
     @endif
 </div>
 @endsection
