3fe57508bb40f784c8408d511c4a1cd648560b51
Baranyai Máté <mateb06@gmail.com>
2020-10-02 12:14:39 +0200

    Add SecureHeaders

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -8,10 +8,7 @@ use Illuminate\Http\Request;
 class SecureHeaders
 {
 
-    private $unwantedHeaderList = [
-        'X-Powered-By',
-        'Server',
-    ];
+    private $unwantedHeaderList = [];
 
     /**
      * Handle an incoming request.
@@ -29,7 +26,7 @@ class SecureHeaders
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-        $response->headers->set('Content-Security-Policy', "style-src 'self'");
+        $response->headers->set('Content-Security-Policy', "font-src 'self' fonts.gstatic.com;style-src 'self' fonts.googleapis.com");
         return $response;
     }
  
