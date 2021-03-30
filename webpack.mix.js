58823c820272e0d3f1415af29e2031a0e02641b2
Baranyai Máté <mateb06@gmail.com>
2021-03-30 18:57:21 +0200

    Update dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css').js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps();
+   .sass('resources/sass/app.scss', 'public/css').js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps().vue();
+
+mix.webpackConfig({
+   resolve: {
+      fallback: {
+         "http": require.resolve("stream-http")
+      }
+   }
+});
